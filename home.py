41271832b9118053b5edef4e447ee1d8cde7d83b
dagrazo <david_agrazo@hotmail.com>
2024-02-08 17:01:03 +0100

    opcion home

--- home.py
+++ home.py
@@ -14,7 +14,7 @@ def home():
     st.markdown("## Primer Proyecto")
     st.markdown('<div style="text-align: justify;">"Solo" habían pasado dos meses desde el inicio, y ya nos teníamos que poner a hacer nuestro propio proyecto, y en colaboración con el resto de compañeros, que locura!!</p></div>', unsafe_allow_html=True)
     st.markdown('<div style="text-align: justify;">Como nos imaginamos que le paso al resto de los compañeros, las dudas de que en aplicar tan "vasto conocimiento" adquirido iban provocando nervios. En nuestro caso dedicamos nuestro primer proyecto a analizar los datos de la API de Open Food Facts</p></div>', unsafe_allow_html=True)
-    st.page_link("https://es.openfoodfacts.org", label="Open Food facts",icon=":female_superhero:")
+    st.page_link("https://es.openfoodfacts.org", label="Open Food facts",icon="🌎")
 
 
 
