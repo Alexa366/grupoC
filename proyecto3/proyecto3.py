115cd6621060dcdeeef2b5b3e38b1d8ab93e7761
dagrazo <david_agrazo@hotmail.com>
2024-02-12 20:35:59 +0100

    cambios proyecto 3

--- proyecto3/proyecto3.py
+++ proyecto3/proyecto3.py
@@ -7,14 +7,13 @@ def proyecto3():
         unsafe_allow_html=True)
     col1, col2 = st.columns(2)
     col1.markdown(
-        '<div style="text-align: justify;">Empezamos nuestro modelo utilizando varios datasets de imagénes de Kaggle, pero los primeros pasos eran dificiles.</p></div>',
+        '<div style="text-align: justify;">Empezamos nuestro modelo utilizando varios datasets de imagénes de Kaggle, pero los primeros pasos eran dificiles. Tanto las graficas de accuracy como de pérdida nos daban mucho overfitting al principio.</p></div>',
         unsafe_allow_html=True)
-    col2.image("image.png", use_column_width="auto")
-    st.markdown(
-        '<div style="text-align: justify;">Probando, probando</p></div>',
+    col2.markdown(
+        '<div style="text-align: justify;">Finalmente cogimos varios datasets de imagénes, llegando a trabajar con 27.441 imagénes</p></div>',
         unsafe_allow_html=True)
     st.markdown(
-        '<div style="text-align: justify;"></p></div>',
+        '<div style="text-align: justify;">Por otro lado, vimos la posibilidad de utilizar la función de Streamlit de camara, para crear un acceso facial a nuestra app</p></div>',
         unsafe_allow_html=True)
 
 
